019676877fc756676e25e6f12deac38e4342442d
Joseph Dennett <rc0038555@student.riversidecollege.ac.uk>
2019-12-09 14:42:45 +0000

    lables

--- trig/Form1.cs
+++ trig/Form1.cs
@@ -16,13 +16,39 @@ namespace trig
         {
             InitializeComponent();
         }
-
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 188.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        double cos(double x)
+        {
+            return Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
         private void PictureBox1_Click(object sender, EventArgs e)
+         
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
